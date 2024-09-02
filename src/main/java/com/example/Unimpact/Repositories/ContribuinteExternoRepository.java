c1ffe38a4b1e0c28087c50069502c0d752567d81
AntonioVital2811 <antonio.00000829963@unicap.br>
2024-09-02 09:39:46 -0300

    UNPC-14 arquivos antigos deletados

--- src/main/java/com/example/Unimpact/Repositories/ContribuinteExternoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Unimpact.Repositories;
-
-import com.example.Unimpact.Models.ContribuinteExterno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContribuinteExternoRepository extends JpaRepository<ContribuinteExterno, Long> {
-}
