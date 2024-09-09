088f81fd05a44de1ab835104722f02bc49503eb8
agcastelao <antonio.00000829625@unicap.br>
2024-09-09 10:57:46 -0300

    UNPC-6 Criação das classes modelo sem implementação

---
+++ demo/src/main/java/com/example/demo/model/TeacherUser.java
@@ -0,0 +1,5 @@
+package com.example.demo.model;
+
+public class TeacherUser extends User {
+    
+}
---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,60 @@
+package com.example.demo.model;
+
+public class User {
+    
+    private Long id;
+    private String password;
+    private String email;
+    private String phone1;
+    private String phone2;
+    private String cnpj;
+    private String cpf;
+
+    public User() {}
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone1() {
+        return phone1;
+    }
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+    public String getPhone2() {
+        return phone2;
+    }
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
+
+}
