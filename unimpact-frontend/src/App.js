d159d96f59b0065d387dbeae41a09f7b679da7ec
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-09-26 23:36:04 -0300

    UNPC-26 Ajustes finais na Criacao da interface de projetos recebidos.

---
+++ unimpact-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; 
+import Recebidos from './components/js/Recebidos'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Recebidos />
+    </div>
+  );
+}
+
+export default App;
